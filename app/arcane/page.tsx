de1bb8f8cf4db1ce8f634a7ab06d463f3b0d47aa
Clevertop <tom@clevertop.dev>
2024-06-21 13:52:14 +1000

    updated arcane link

--- app/arcane/page.tsx
+++ app/arcane/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-    redirect('https://cookiespl.itch.io/arcane-raiders') // working
+    //redirect('https://cookiespl.itch.io/arcane-raiders') // itch page
+    redirect('https://store.steampowered.com/app/2899410/Arcane_Raiders/') // steam page
 }
