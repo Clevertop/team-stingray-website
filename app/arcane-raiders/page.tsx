1ab93b8d19f3b489797fb0920cdfee1605f148e0
Clevertop <tom@clevertop.dev>
2024-06-24 21:31:22 +1000

    added a landing page for the tech fest links

---
+++ app/arcane-raiders/page.tsx
@@ -0,0 +1,32 @@
+
+
+
+export default async function ArcaneRaiders() {
+    // server code
+    return (
+        <div className="landing-div">
+            {/* <h1 className="landing-heading">Arcane Raiders</h1> */}
+            
+            <img className="image-card" src="https://img.itch.zone/aW1nLzE2Mjg0ODg2LnBuZw==/original/ysnCCU.png"></img>
+
+            <br></br>
+
+            <a href="https://store.steampowered.com/app/2899410/Arcane_Raiders/">
+                <div className="landing-card" >
+                    <h2 className="landing-subheading">Steam</h2>
+                    <p>Wishlist the game!</p>
+                </div>
+            </a>
+
+            <br></br>
+
+            <a href="https://cookiespl.itch.io/arcane-raiders">
+                <div className="landing-card" >
+                    <h2 className="landing-subheading">Itch.io</h2>
+                    <p>Play the game now!</p>
+                </div>
+            </a>
+            
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -59,24 +59,24 @@ export default function Home() {
           </p>
         </a>
 
-        {/* <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        <a
+          href="/arcane-raiders"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
+            Landing{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Landing page for texh fest link
           </p>
         </a>
 
-        <a
+        {/* <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
